274ca8d891caeaa69fd45df936bc764d826a6df4
Andrewki44 <andrewkilbourn1996@gmail.com>
2025-12-05 16:37:09 +1100

    v0.10.0

--- scripts/logic/core.lua
+++ scripts/logic/core.lua
@@ -26,6 +26,50 @@ function hasSwimmers(quantity)
     end
 end
 
+function hasPartyMembers(quantity)
+    local quantity = tonumber(quantity)
+    local counter = 0
+
+    if hasTidus() then
+        counter = counter + 1
+        print("counter: " + counter)
+    end
+
+    if hasWakka() then
+        counter = counter + 1
+    end
+
+    if hasLulu() then
+        counter = counter + 1
+    end
+
+    if hasYuna() then
+        counter = counter + 1
+    end
+
+    if hasKimahri() then
+        counter = counter + 1
+    end
+
+    if hasAuron() then
+        counter = counter + 1
+    end
+
+    if hasRikku() then
+        counter = counter + 1
+    end
+
+    if hasSeymour() then
+        counter = counter + 1
+    end
+
+    if counter >= quantity then
+        return true
+    else
+        return false
+    end
+end
+
 function superBosses()
     if has("superbosses") then
         return true
