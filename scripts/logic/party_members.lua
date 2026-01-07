20d27c389a7c582a5619edd00099d33f9f6dc2e2
Andrewki44 <andrewkilbourn1996@gmail.com>
2026-01-08 07:20:20 +1100

    Update MinPartyRule

--- scripts/logic/party_members.lua
+++ scripts/logic/party_members.lua
@@ -247,3 +247,42 @@ function hasPartyMembersAndAeons(quantity)
         return false
     end
 end
+
+function hasMinimumParty(quantity)
+    local quantity = tonumber(quantity)
+    local counter = 0
+
+    if hasTidus() then
+        counter = counter + 1
+    end
+
+    if hasWakka() then
+        counter = counter + 1
+    end
+
+    if hasLulu() then
+        counter = counter + 1
+    end
+
+    if hasYuna() then
+        counter = counter + 1
+    end
+
+    if hasKimahri() then
+        counter = counter + 1
+    end
+
+    if hasAuron() then
+        counter = counter + 1
+    end
+
+    if hasRikku() then
+        counter = counter + 1
+    end
+
+    if counter >= quantity then
+        return true
+    else
+        return false
+    end
+end
